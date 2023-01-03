3d87a570a054b9c2fe24a6ba602d3ae24e22d57c
Avinash Kumar Singh <crazydinclub@gmail.com>
2023-01-04 01:58:52 +0530

    New file adding dynamic typing.

---
+++ Python fundamentals/Dynamic typing.py
@@ -0,0 +1,5 @@
+#Here in dynamic typing  python keep on assigning new values and stores the values to variables.
+x= 12
+print(x)                 #values of x=12 stores in it and print the same.
+x= "Hello world welcome to dynamic typing"
+print(x)
