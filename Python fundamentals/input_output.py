eb72c7847720f1aa4276ecd137821fca1dc950fa
Avinash Kumar Singh <crazydinclub@gmail.com>
2023-01-04 12:32:21 +0530

    explaining about simple input ouput in python third commit

---
+++ Python fundamentals/input_output.py
@@ -0,0 +1,8 @@
+name =input('What is your name ?')
+print('Your name:', name)
+
+age = input('Enter your age :')
+print('You entered:', age)
+
+location = input('Enter your location :')
+print('Your location:', location)
