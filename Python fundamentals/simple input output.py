caaa57ad13cf8ee4029c950f36aaf21dc6e3377e
Avinash Kumar Singh <crazydinclub@gmail.com>
2023-01-05 11:50:24 +0530

    new file adding simple input output function

---
+++ Python fundamentals/simple input output.py
@@ -0,0 +1,14 @@
+#simple input output FOR input this function is used 'input()'
+name = input('What is your name?')           #the input() function is used to take value for name
+    
+age = input('Enter your age')                #the input() function is used to take value for age
+
+favfood = input('Enter your fav food')       #the input() function is used to take value for favfood
+
+print(name)                                         # entered name is displayed 'print()'
+
+print(age)                                          # entered age is displayed                     
+
+print(favfood )                                     # entered favfood is displayed                    
+
+
