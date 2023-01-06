8ebfcdab4588abf34c54dd480233f02b06f63dc3
Avinash Kumar Singh <crazydinclub@gmail.com>
2023-01-06 23:19:23 +0530

    2 questions on python program.

---
+++ Python fundamentals/practice ques.py
@@ -0,0 +1,22 @@
+#Solving the followings programs.
+
+# 01. Write a program to obtain three numbers and print their sum.
+print("01. Write a program to obtain three numbers and print their sum.")
+number1= int(input('Enter number1:'))
+number2= int(input('Enter number2:'))
+number3= int(input('Enter number3:'))
+sum= number1+number2+number3
+print("Three numbers are",'number1','number2','number3')
+print("Sum of three number is", sum)
+
+
+#02. Write a program to obtain length and breadth of a rectangle and calculate its area.
+print("02. Write a program to obtain length and breadth of a rectangle and calculate its area.")
+
+length= float(input('Enter the length of the rectangle:'))
+breadth= float(input('Enter the breadth of the rectangle:'))
+area=length*breadth
+print("Rectangele dimensions")
+print('Length=', length, end=' ' )
+print('Breadth=', breadth )
+print('Area', area,) 
