b10ef1d31a07a44629da33807321ce2f2e973037
Avinash Kumar Singh <crazydinclub@gmail.com>
2023-01-06 12:16:21 +0530

    variable favfood is added, A comment message is included in this file.

--- Python fundamentals/input_output.py
+++ Python fundamentals/input_output.py
@@ -1,3 +1,5 @@
+# Input output with print() function. Here the input() is displayed by print function Print()
+
 name =input('What is your name ?')
 print('Your name:', name)
 
@@ -6,3 +8,6 @@ print('You entered:', age)
 
 location = input('Enter your location :')
 print('Your location:', location)
+
+favfood = input('write your favfood name')
+print('Your favfood:', favfood)
