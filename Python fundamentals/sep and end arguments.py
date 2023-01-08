030487b00ad5dbdfa12bfc198b512f562faa8a4c
Avinash Kumar Singh <crazydinclub@gmail.com>
2023-01-08 18:31:43 +0530

    Arguments explained.

---
+++ Python fundamentals/sep and end arguments.py
@@ -0,0 +1,8 @@
+#Explainig the sep and end arguments.
+print('A','N', sep='', end='')            # end='' place the next following line of code (I)."
+print('I')
+#\n provides new line after printing the year
+print('08','01','2023', sep='-', end='\n')
+
+print('Red','Green','Blue', sep='...', end='@')
+print('Is my favourite color.')
